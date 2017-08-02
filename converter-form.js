a483a18e8c928d1dfca3a8677b3dc4f1de29dbdc
Bertrand the Healer <17074869+BertrandTheHealer@users.noreply.github.com>
2017-08-02 14:51:54 -0600

    Update converter-form.js

--- converter-form.js
+++ converter-form.js
@@ -122,7 +122,7 @@ if (typeof ConverterForm !== 'object') {
 
         // Turn the array to a sentence, and update the DOM
         IPAText = IPAText.join(' ');
-        IPAText = IPAText.replace('j', 'y').replace("ˈ", '');
+        IPAText = IPAText.replace('j', 'y');
 
         // Make sure the output ID exists before outputting IPA
         if (typeof outID === 'string') {
